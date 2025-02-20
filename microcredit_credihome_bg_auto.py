8bb17e068aa234438f5380f832afbcd7de4799d3
Nikolai Raikov <nikisuhoto@gmail.com>
2025-02-20 11:51:04 +0200

    def

--- microcredit_credihome_bg_auto.py
+++ microcredit_credihome_bg_auto.py
@@ -4,54 +4,65 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time as tm
 
 n = int(input("Time to sleep?"))
-
+element = None
 driver = webdriver.Chrome()
 driver.get("https://old-microcredit.credihome.bg/")
 tm.sleep(n)
 
-# <input class="StandardInput StandardInput-is-block EmailInput" type="email"
-# name="email">
 
-element = driver.find_element("name", "email")
-element.send_keys("niki@dron.bg")
-tm.sleep(n)
-element.send_keys(Keys.ENTER)
+def verification(n):
+    # <input class="StandardInput StandardInput-is-block EmailInput" type="email"
+    # name="email">
 
-tm.sleep(30)
-# number_code = int(input("Number code:"))
-
-try:
-    element = driver.find_element("name", "code")
-
+    element = driver.find_element("name", "email")
+    element.send_keys("niki@dron.bg")
     tm.sleep(n)
     element.send_keys(Keys.ENTER)
 
-except:
-    pass
+    tm.sleep(30)
+    # number_code = int(input("Number code:"))
 
-### id="AmountIndex" max="43" min="0" name="AmountIndex" step="1"
-# data-val-required="The MonthsIndex field is required." id="MonthIndex" max="11" min="0" name="MonthsIndex"
+    try:
+        element = driver.find_element("name", "code")
 
-slider = driver.find_element("id", "MonthIndex")
-actions = ActionChains(driver)
-actions.click_and_hold(slider).move_by_offset(10, 0).release().perform()
-# [3,4,5,6,7,8,9,10,11,12,15,18]
-tm.sleep(n)
+        tm.sleep(n)
+        element.send_keys(Keys.ENTER)
 
-# id="email_txt" name="Email" placeholder="Имейл"
+    except:
+        pass
 
-element = driver.find_element("id", "email_txt")
-element.send_keys("niki@dron.bg")
-tm.sleep(n)
 
-# id="phone_txt" name="Phone" placeholder="Телефон"
+def slider_left(n):
+    ### id="AmountIndex" max="43" min="0" name="AmountIndex" step="1"
+    # data-val-required="The MonthsIndex field is required." id="MonthIndex" max="11" min="0" name="MonthsIndex"
 
-element = driver.find_element("id", "phone_txt")
-element.send_keys("0878734442")
-tm.sleep(n)
+    slider = driver.find_element("id", "MonthIndex")
+    actions = ActionChains(driver)
+    actions.click_and_hold(slider).move_by_offset(10, 0).release().perform()
+    # [3,4,5,6,7,8,9,10,11,12,15,18]
+    tm.sleep(n)
 
-# <button data-action="save" type="submit" id="fix-form-btn">Вземи сега</button>
+    # id="email_txt" name="Email" placeholder="Имейл"
 
-element = driver.find_element("id", "fix-form-btn")
-element.click()
-tm.sleep(n*5)
+    element = driver.find_element("id", "email_txt")
+    element.send_keys("niki@dron.bg")
+    tm.sleep(n)
+
+
+def credit_take_now(n):
+    # id="phone_txt" name="Phone" placeholder="Телефон"
+
+    element = driver.find_element("id", "phone_txt")
+    element.send_keys("0878734442")
+    tm.sleep(n)
+
+    # <button data-action="save" type="submit" id="fix-form-btn">Вземи сега</button>
+
+    element = driver.find_element("id", "fix-form-btn")
+    element.click()
+    tm.sleep(n * 5)
+
+
+verification(n)
+slider_left(n)
+credit_take_now(n)
